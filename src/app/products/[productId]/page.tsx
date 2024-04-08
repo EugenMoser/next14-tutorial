8eb26e1043cc6a12a9f4f133c246beb4752da82b
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-08 15:51:43 +0200

    7 - Dynamic Routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+interface ProductDetailsProps {
+  params: { productId: number };
+}
+
+function ProductDetails({ params }: ProductDetailsProps): JSX.Element {
+  return <h1>Product Details of ID {params.productId}</h1>;
+}
+
+export default ProductDetails;
