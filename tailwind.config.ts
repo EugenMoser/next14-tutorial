007143f01c0252e4611b31cbc28ade6cf0cfada3
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-08 17:18:13 +0200

    14 - Layouts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,35 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+      gray: {
+        "100": "#f7fafc",
+        // ...
+        "900": "#1a202c",
+      },
+      red: "#EF4444",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        display: "Oswald, ui-serif", // Adds a new `font-display` class
+      },
     },
   },
+
   plugins: [],
 };
 export default config;
