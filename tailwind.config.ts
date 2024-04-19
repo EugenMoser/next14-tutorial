d3242775a5cdf3e23b7356fa0c3c160202a957e7
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-19 13:52:16 +0200

    20 - Active Links

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      transparent: "transparent",
-      black: "#000",
-      white: "#fff",
-      gray: {
-        "100": "#f7fafc",
-        // ...
-        "900": "#1a202c",
-      },
-      red: "#EF4444",
-    },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
     },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        display: "Oswald, ui-serif", // Adds a new `font-display` class
+        display: 'Oswald, ui-serif', // Adds a new `font-display` class
       },
     },
   },
