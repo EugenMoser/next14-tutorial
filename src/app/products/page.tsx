61bf1041dc9c50e788f09468fa4443d931819266
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-19 13:22:32 +0200

    19 - Link Component Navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,30 @@
+import Link from 'next/link';
+
 interface ProductListProps {}
 
 function ProductList({}: ProductListProps): JSX.Element {
+  const productId = 100;
   return (
     <>
+      <Link href='/'>Home</Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href='products/1'>Product 1</Link>
+      </h2>
+      <h2>
+        <Link href='products/2'>Product 2</Link>
+      </h2>
+      <h2>
+        <Link
+          href='products/3'
+          replace //replace = true, ersetzt den aktuellen Eintrag im Verlauf durch den neuen Eintrag
+        >
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
   );
 }
