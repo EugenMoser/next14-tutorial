f3a44a6a0f99ab8232343eda934764dec50c0482
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-19 12:39:50 +0200

    17 - Routing Metadata - static

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+export const metadata = {
+  title: 'About Schenka Penka',
+  description: 'About description',
+};
+
 interface AboutProps {}
 
 function About({}: AboutProps): JSX.Element {
-  return <div>about</div>;
+  return <h1>about</h1>;
 }
 
 export default About;
