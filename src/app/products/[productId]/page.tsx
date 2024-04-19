8df21883ca96b692362842d487502747847ec146
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-19 12:53:57 +0200

    17 - Routing Metadata - dynamic

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,19 @@
-interface ProductDetailsProps {
+import { Metadata } from 'next';
+
+type ProductDetailsProps = {
   params: { productId: string };
-}
+};
+
+export const generateMetadata = async ({
+  params,
+}: ProductDetailsProps): Promise<Metadata> => {
+  const title = await new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 100);
+  });
+  return { title: `Product  ${title}` };
+};
 
 function ProductDetails({ params }: ProductDetailsProps): JSX.Element {
   return <h1>Product Details of ID {params.productId}</h1>;
