bcf15e7b420cb14d9f90db6f647ae078cd53655f
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-19 13:11:23 +0200

    18 - title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Blog', //angezeigt wird Blog | Next.js Tutorial, da aus übergeordneten Layout der Zusatz hinzugefügt wird
+  // title:{
+  //   absolute: 'Blog', //absolute title = dieser Wert wird immer gesetzt und ignoriert die übergoordneten tempaltes
+  // }
+};
+
 interface BlogProps {}
 
 function Blog({}: BlogProps): JSX.Element {
