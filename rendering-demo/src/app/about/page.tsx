81e6e68128706023db9a43d289384b8bf68c7516
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-06-07 23:54:00 +0200

    52 - Static Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -6,7 +6,7 @@ function About({}: AboutProps): JSX.Element {
   console.log('About server Compoenent');
   return (
     <>
-      <h1>about page</h1>
+      <h1>about page {new Date().toLocaleTimeString()}</h1>
     </>
   );
 }
